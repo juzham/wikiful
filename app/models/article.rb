902607f8c6cb2b9d85a8e0095ab6de3e4f532431
Justin Hamman <justinhamman@gmail.com>
2014-01-16 16:50:15 +1100

    added a new model ArticleCategory

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ActiveRecord::Base
+	belongs_tp :user
+	has_many :categories, through: :article_categories
+	validates :tile presence: true
+	validates :content, presence: true
+	validates :categories, presence: true
 end
