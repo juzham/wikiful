db319f05807f1d6aba76f6f9629bb2147f76e0b9
Justin Hamman <justinhamman@gmail.com>
2014-01-20 17:41:06 +1100

    built out category functionality, no add yet though

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,8 @@ class CategoriesController < ApplicationController
 	end
 
 	def show
+		@category = Category.find(params[:id])
+		@articles = @category.articles.order(created_at: :desc)
 	end
 
 	def new
